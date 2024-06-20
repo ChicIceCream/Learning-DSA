64fdad5de5583ad215292f3c1b6e81ac660de699
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2024-06-20 16:31:24 +0530

    learend linked list

--- Linked List.py
+++ Linked List.py
@@ -1,25 +1,25 @@
 class Node:
-    def __init__(self, data=None, next=None):
+    def __init__(self, data):
         self.data = data
         self.next = None
-        
-class linked_list:
-    def __init__(self):
-        self.head = None 
+    
+# Start creating nodes
+# 1st node
+node1 = Node(10)
+# 2nd node
+node2 = Node(20)
+# 3rd node
+node3 = Node(30)
+# 4th node
+node4 = Node(40)
 
-    def insert_at_begining(self, data):
-        node = Node(data), self.head
-        self.head = node 
-        
-    def print(self):
-        if self.head is None:
-            print("Linked list is empty")
-            return
-        itr = self.head
-        while itr:
-            itr = itr.next
-        
+node1.next = node2
+node2.next = node3
+node3.next = node4
 
-if __name__ == '__main__':
-    pass
-        
+# Printing the linked list
+current = node1
+while current is not None:
+    print(f'{current.data}', end='->')
+    current = current.next
+print(f'None')
