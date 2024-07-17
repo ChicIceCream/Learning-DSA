9b40ff0c824b8fa248eadd90975cb0d8942ec5e1
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2024-07-17 18:22:54 +0530

    learning stack

---
+++ Stack.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if len(s) == 0:
+            return True
+
+        stack = []
+        bracket_mapping = {')': '(', ']': '[', '}': '{'}
+
+        for char in s:
+            print(stack)
+            if char in bracket_mapping:
+                if stack and stack[-1] == bracket_mapping[char]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(char)
+
+        return not stack
+
+solution = Solution()
+print(solution.isValid("()"))      # Output: True
+# print(solution.isValid("()[]{}"))  # Output: True
+# print(solution.isValid("(]"))      # Output: False
+# print(solution.isValid("([)]"))    # Output: False
+# print(solution.isValid("{[]}"))    # Output: True
+
